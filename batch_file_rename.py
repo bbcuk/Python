d53570e6a13816a0c93dfc5dd9fd79fea96fbac9
bbcuk <lemonwolfergky@gmail.com>
2017-03-24 10:17:10 +0800

    Update batch_file_rename.py

--- batch_file_rename.py
+++ batch_file_rename.py
@@ -1,64 +1,46 @@
 # batch_file_rename.py
-# Created: 6th August 2012
+# Created: 2017 3 24
 
 '''
 This will batch rename a group of files in a given directory,
 once you pass the current and new extensions
 '''
 
-__author__  = 'Craig Richards'
+__author__  = 'Li Chao'
 __version__ = '1.0'
-
-import os
 import sys
+import os.path
 import argparse
-
-def batch_rename(work_dir, old_ext, new_ext):
-    '''
-    This will batch rename a group of files in a given directory,
-    once you pass the current and new extensions
-    '''
-    # files = os.listdir(work_dir)
-    for filename in os.listdir(work_dir):
-        # Get the file extension
-        file_ext = os.path.splitext(filename)[1]
-        # Start of the logic to check the file extensions, if old_ext = file_ext
-        if old_ext == file_ext:
-            # Returns changed name of the file with new extention
-            name_list=list(filename)
-            name_list[len(name_list)-len(old_ext):]=list(new_ext)
-            newfile=''.join(name_list)
-            
-            # Write the files
-            os.rename(
-                os.path.join(work_dir, filename),
-                os.path.join(work_dir, newfile)
-            )
 
 def get_parser():
     parser = argparse.ArgumentParser(description='change extension of files in a working directory')
     parser.add_argument('work_dir', metavar='WORK_DIR', type=str, nargs=1, help='the directory where to change extension')
-    parser.add_argument('old_ext', metavar='OLD_EXT', type=str, nargs=1, help='old extension')
     parser.add_argument('new_ext', metavar='NEW_EXT', type=str, nargs=1, help='new extension')
     return parser
 
+def batch_file_rename(rootdir,ext):
+	if not os.path.isdir(rootdir):
+		print("The directory is invalid:",rootdir)
+		return
+	for parent,dirnames,filenames in os.walk(rootdir):
+
+		for filename in filenames:
+			print("file: ",parent,filename,os.path.join(parent,filename))	
+			newfilename = "{0}.{1}".format(os.path.splitext(filename)[0],ext)
+			os.rename(os.path.join(parent,filename),os.path.join(parent,newfilename))
+
 def main():
-    '''
-    This will be called if the script is directly invoked.
-    '''
-    # adding command line argument
+	    # adding command line argument
     parser = get_parser()
     args = vars(parser.parse_args())
 
     # Set the variable work_dir with the first argument passed
     work_dir = args['work_dir'][0]
-    # Set the variable old_ext with the second argument passed
-    old_ext = args['old_ext'][0]
+
     # Set the variable new_ext with the third argument passed
     new_ext = args['new_ext'][0]
-
-    batch_rename(work_dir, old_ext, new_ext)
-
+    batch_file_rename(work_dir,new_ext)
 
 if __name__ == '__main__':
-    main()
+	main()
+
